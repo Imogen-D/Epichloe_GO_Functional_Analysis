8753feaa7d8a25bc0ff062d4808baf44bb4bfc2c
Imogen Dumville <57735780+Imogen-D@users.noreply.github.com>
2020-02-27 11:33:06 +1300

    Update and rename MakingFinalFrame.R to scripts/3_MakingFinalFrame.R

--- scripts/3_MakingFinalFrame.R
+++ scripts/3_MakingFinalFrame.R
@@ -1,18 +1,19 @@
-ortho_long <- read.delim("~/CoxExtension/ortho_long.tsv", header=FALSE, stringsAsFactors=FALSE)
-Eamquant <- read.delim("~/CoxExtension/RNA/quantfiles/Eamquant.sf", stringsAsFactors=FALSE)
+ortho_long <- read.delim("ortho_long.tsv", header=FALSE, stringsAsFactors=FALSE)
+#quant files from full quantification in https://github.com/Imogen-D/Cyclic-Peptide-Expression
+Eamquant <- read.delim("Eamquant.sf", stringsAsFactors=FALSE)
 
 Eam702 <- ortho_long[which(ortho_long$V3 == "Eam702"),]
 colnames(Eam702) <- c("ortholog", "Name", "Strain")
 mergedTPMandid <- merge(Eam702, Eamquant)
 
-amarillansuniquegeneid <- read.table("~/CoxExtension/LineageSpecificity/amarillansuniquegeneid.tsv", quote="\"", comment.char="", stringsAsFactors=FALSE)
+amarillansuniquegeneid <- read.table("amarillansuniquegeneid.tsv", quote="\"", comment.char="", stringsAsFactors=FALSE)
 uniquegenes <- amarillansuniquegeneid$V1
 geneList <- factor(as.integer(Eam702$Name %in% uniquegenes))
 names(geneList) <- Eam702$Name
 TFgenes <- data.frame(TF = geneList, Name = Eam702$Name, row.names = NULL)
 mergedTPMandTF <- merge(mergedTPMandid, TFgenes)
 
-Amaclosest_AT <- read.delim("~/CoxExtension/Amaclosest_AT.tsv", header=FALSE, stringsAsFactors=FALSE)
+Amaclosest_AT <- read.delim("Amaclosest_AT.tsv", header=FALSE, stringsAsFactors=FALSE)
 library(stringr)
 Amaclosest_AT$V1 <- str_remove(Amaclosest_AT$V1, "ID=")
 Amaclosest_AT$V1 <- str_replace(Amaclosest_AT$V1, ";", "-T1")
@@ -26,11 +27,9 @@ write.csv(Amarillansfinalframe, file = "AmarillansData.csv")
 
 ##for Bromicola
 
-ortho_long <- read.delim("~/CoxExtension/ortho_long.tsv", header=FALSE, stringsAsFactors=FALSE)
-
 EbroNfe1 <- ortho_long[which(ortho_long$V3 == "EbroNfe1"),]
 colnames(EbroNfe1) <- c("ortholog", "Name", "Strain")
-bromicolauniquegeneid <- read.table("~/CoxExtension/LineageSpecificity/bromicolauniquegeneid.tsv", quote="\"", comment.char="", stringsAsFactors=FALSE)
+bromicolauniquegeneid <- read.table("bromicolauniquegeneid.tsv", quote="\"", comment.char="", stringsAsFactors=FALSE)
 uniquegenes_brom <- bromicolauniquegeneid$V1
 geneList_brom <- factor(as.integer(EbroNfe1$Name %in% uniquegenes_brom))
 names(geneList_brom) <- EbroNfe1$Name
@@ -38,9 +37,9 @@ TFgenes_brom <- data.frame(TF = geneList_brom, Name = EbroNfe1$Name, row.names =
 merged_brom <- merge(EbroNfe1, TFgenes_brom)
 #no length
 
-Ebroclosest_AT <- read.table("~/CoxExtension/BromicolaATclosest.tsv", quote="\"", stringsAsFactors=FALSE)
+Ebroclosest_AT <- read.table("BromicolaATclosest.tsv", quote="\"", stringsAsFactors=FALSE)
 colnames(Ebroclosest_AT) <- c("Name", "DistanceAT")
-BromicolaLengths <- read.delim("~/CoxExtension/BromicolaLengths.csv", header=FALSE, stringsAsFactors=FALSE)
+BromicolaLengths <- read.delim("BromicolaLengths.csv", header=FALSE, stringsAsFactors=FALSE)
 colnames(BromicolaLengths) <- c("Name", "Length")
 
 BromicolaAT <- merge(Ebroclosest_AT, merged_brom)
@@ -53,6 +52,7 @@ Amarillansfinalframe$TF <- str_replace(Amarillansfinalframe$TF, "1", "TRUE")
 BromicolaFinalFrame$TF <- str_replace(BromicolaFinalFrame$TF, "0", "FALSE")
 BromicolaFinalFrame$TF <- str_replace(BromicolaFinalFrame$TF, "1", "TRUE")
 
+#graphs
 boxplot(Amarillansfinalframe$Length~Amarillansfinalframe$TF,
         data=Amarillansfinalframe,
         main="Boxplot of Lineage Specificity and Protein Length",
@@ -104,6 +104,7 @@ boxplot(BromicolaFinalFrame$Length~BromicolaFinalFrame$TF,
         horizontal = TRUE
 )
 
+#stats
 mod <- glm(TPM < 2 ~ TF, data=Amarillansfinalframe, family = "binomial")
 summary(mod) #SIGNFICANT
 mod <- glm(DistanceAT < 2000 ~ TF, data=Amarillansfinalframe, family = "binomial")
